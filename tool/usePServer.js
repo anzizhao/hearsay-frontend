85329c9dd0f305b5647d955dcff87625a471d739
anzizhao <anzizhao@126.com>
2016-12-01 12:19:34 +0800

    修改脚本

--- tool/usePServer.js
+++ tool/usePServer.js
@@ -23,7 +23,7 @@ var transformMap = [
     //},
     {
         host: 'www.wired.com',
-        category: "Wired.com",
+        category: "WiredScience",
     },
     //{
         //host: "blog.jobbole.com",
@@ -54,41 +54,25 @@ if( process.env.NODE_DEV === 'development') {
     g_pServerUrl = "https://image.anzizhao.com/api/v1/picture/fetch";
 }
 
+
+
+// 对pServer 返回404的项 重新请求 
 setTimeout(function(){
+    reFetch404Item();
+    //usePServer();
+}, 1000)
 
+
+// 对pServer 返回404的项 重新请求 
+function reFetch404Item () {
     var HearsayModel = models.hearsay.Entry;
-    //var query =  {
-        //"host": "mashable.com",
-    //}
-
-    //var fields = '-content.body';
-    //HearsayModel.find( query ,fields,  function(err, items) {
-        //if (err) {
-            //console.dir( err );
-            //debug( err );
-            //return 
-        //}
-        //if ( !items.length ) {
-            //return  console.error('no item found');
-        //}
-        //debug( 'find somthing ');
-        //items.forEach( function(item, index){
-            //console.dir( item )
-            //debug( item );
-        //})
-    //})
-
+    var gPicUrl =  "https://image.anzizhao.com/picture";
     var fields = '-content.body';
     var query =  {
-        //'host': 'www\.wired\.com',
-        //"host": "mashable.com",
         "host": { 
             "$in": hosts 
         }
-        //source: "https://www.wired.com",
-        //category: 'wired', 
     }
-
     HearsayModel.find( query, fields,  function(err, items) {
         if (err) {
             console.dir( err );
@@ -98,42 +82,88 @@ setTimeout(function(){
             return  console.error('no item found');
         }
         items.forEach( function(item, index){
-            if( ! item.image ) {
-                return  
+            //console.log('item: ', item.url, item.imageB )
+            if( ! item.imageB ) {
+                return 
             }
-            //if( item.image.indexOf( '/images/') !== -1  ) {
-                //console.log('not to deal: ', item.image);
-                //return  
-            //}
-            //return console.log(item.host, item.image );
-
-            var url = util.fixRelativePath( item.image, item.source);
-            var requestData = {
-                url: url,
-                category: hostMapCategory[item.host],
-                options: {
-                    webp: true, 
-                }
-            };
             request({
-                url: g_pServerUrl,
-                method: "POST",
-                json: true,
-                body: requestData
+                url: 'https:' + item.imageB,
+                method: "GET",
             }, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log('result: ', body.data.url );
-                    item.imageB = body.data.url;
-                    item.save();
+                if ( error || response.statusCode !== 404) {
                     return 
                 }
-
-                console.log( g_pServerUrl, error, response.statusCode);
+                console.log('404 item', item.imageB )
+                // 返回404 项, 重现抓取
+                makeFetchRequest(item);
             }); 
 
+              //return console.log(item.host, item.image );
+
+        })
+        console.log('finish')
+    })
+}
+
+
+
+
+// 将需要的网站的图片使用pServer
+function usePServer ( ){
+    var HearsayModel = models.hearsay.Entry;
+    var fields = '-content.body';
+    var query =  {
+        "host": { 
+            "$in": hosts 
+        }
+    }
+    HearsayModel.find( query, fields,  function(err, items) {
+        if (err) {
+            console.dir( err );
+            return 
+        }
+        if ( !items.length ) {
+            return  console.error('no item found');
+        }
+        items.forEach( function(item, index){
+            if( ! item.image 
+                || item.imageB 
+                ||( item.host.indexOf('jobbole.com') !== -1 && item.image.indexOf('/images') !== -1 ) 
+              ) 
+                    {
+                        return  
+                    }
+
+
+                    makeFetchRequest(item);
         })
     })
+}
 
-}, 1000)
 
 
+function makeFetchRequest(item) {
+    var url = util.fixRelativePath( item.image, item.source);
+    var requestData = {
+        url: url,
+        category: hostMapCategory[item.host],
+        options: {
+            webp: true, 
+        }
+    };
+    request({
+        url: g_pServerUrl,
+        method: "POST",
+        json: true,
+        body: requestData
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('result: ', body.data.url );
+            item.imageB = body.data.url;
+            item.save();
+            return 
+        }
+
+        console.log( g_pServerUrl, error, response.statusCode);
+    }); 
+}
